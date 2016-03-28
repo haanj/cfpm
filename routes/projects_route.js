24c9036119a2aa18748da2a07ab4f184d661f7b6
haanj <joshua.haan@ymail.com>
2016-03-28 14:34:37 -0700

    fixes multipart parsing

--- routes/projects_route.js
+++ routes/projects_route.js
@@ -3,6 +3,7 @@
 // let authenticate = require('../lib/authenticate')
 let fs = require('fs')
 let formidable = require('formidable')
+let util = require('util')
 
 module.exports = (router) => {
 
@@ -12,31 +13,30 @@ module.exports = (router) => {
       res.json('Hello login')
     })
     // .post((req, res) => console.log('post to /projects'))
-    // .post((req, res, next) => {
-    //   let form = new formidable.IncomingForm();
-    //
-    //   form.parse(req, function(err, fields, files) {
-    //     res.writeHead(200, {'content-type': 'text/plain'});
-    //     res.write('received upload:\n\n');
-    //     res.end(util.inspect({fields: fields, files: files}));
-    //   });
-    //   form.on('file', function(name, file) {
-    //     console.log(name, file)
-    //   })
-    //
-    //   form.on('end', () => {
-    //     next();
-    //   })
-    //
-    // })
-    .post((req, res) => {
-      let body = ''
-      req.on('data', (chunk) => {
-        body += chunk
-      })
-      req.on('end', () => {
-        console.log(req.body)
-        fs.writeFile(__dirname + '/../data/'+req.headers.filename, body, () => res.json('post received'))
-      })
+    .post((req, res, next) => {
+      let form = new formidable.IncomingForm()
+      form.uploadDir = __dirname + '/../data/'
+
+      form.parse(req, function(err, fields, files) {
+        console.log(files)
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields: fields, files: files}));
+      });
+
+      // form.on('end', () => {
+      //   res.json('thanks')
+      // })
+
     })
+    // .post((req, res) => {
+    //   let body = ''
+    //   req.on('data', (chunk) => {
+    //     body += chunk
+    //   })
+    //   req.on('end', () => {
+    //     console.log(body)
+    //     fs.writeFile(__dirname + '/../data/'+req.headers.filename, body, () => res.json('post received'))
+    //   })
+    // })
 }
